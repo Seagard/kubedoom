0098f94910c708f07958efef007028490ade9c08
Christopher Melgard <chris.melgard@braingu.com>
2023-03-27 22:34:12 -0500

    add cluster bigbang

--- clusters/minikube/scripts/bootstrap.sh
+++ clusters/minikube/scripts/bootstrap.sh
@@ -2,3 +2,4 @@
 minikube start --driver=hyperkit 
 export GITHUB_TOKEN=$(security find-internet-password -s github.com -w)
 flux bootstrap github --owner=seagard --branch=flux --repository=kubedoom --path=clusters/minikube --personal
+kubectl apply -f clusters/minikube/bigbang
