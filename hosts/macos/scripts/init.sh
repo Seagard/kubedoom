9762dc6a345a3a5be35ba69ebfbb8a21cbde4a2a
Christopher Melgard <chris.melgard@braingu.com>
2023-03-28 13:41:43 -0500

    two gitrepos

--- hosts/macos/scripts/init.sh
+++ hosts/macos/scripts/init.sh
@@ -12,4 +12,4 @@ helm registry login registry1.dso.mil \
 -u $(security find-generic-password -s registry1.dso.mil |rg '.*acc.*"(.*)"$' -r '$1') \
 -p $(security find-generic-password -s registry1.dso.mil -w)
 
-# minikube start --driver=hyperkit
+minikube start --driver=hyperkit
