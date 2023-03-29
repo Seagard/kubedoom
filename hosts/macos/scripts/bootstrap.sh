6e6c18c9dff1722d4bc2722cd0bff51014b7c87f
Christopher Melgard <chris.melgard@braingu.com>
2023-03-29 10:40:20 -0500

    add submodule

--- hosts/macos/scripts/bootstrap.sh
+++ hosts/macos/scripts/bootstrap.sh
@@ -1,4 +1,4 @@
 #!zsh
 export GITHUB_TOKEN=$(security find-internet-password -s github.com -w)
-flux bootstrap github --owner=seagard --branch=flux --repository=kubedoom --path=clusters/bootstrap --personal
+flux bootstrap github --owner=seagard --branch=flux --repository=kubedoom --path=clusters/bootstrap --personal --recurse-submodules
 kubectl apply -f clusters/minikube/bigbang
